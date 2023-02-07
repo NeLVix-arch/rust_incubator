7537b602010265060eb23b03803bf1de41d07ada
NeLVix-arch <for9yo@gmail.com>
2023-02-07 13:53:02 +0300

    first step

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Implement me!");
+use smart_default::SmartDefault;
+
+#[derive(Clone, Copy, Default)]
+struct Point {
+    x: i32,
+    y: i32,
 }
+
+#[derive(Clone, SmartDefault)]
+enum Polyline {
+    #[default]
+    Point {
+        #[default = 1]
+        x: i32,
+        y: i32,
+    }
+}
+
+fn main() {}
